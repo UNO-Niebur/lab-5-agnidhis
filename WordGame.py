122dd6bc31510670d3c59d5cd26a93576ca1c0bc
agnidhis <nidhiagarwal@unomaha.edu>
2026-02-20 15:04:03 +0000

    Word game

--- WordGame.py
+++ WordGame.py
@@ -4,19 +4,36 @@ import random
 
 def inWord(letter, word):
     """Returns boolean if letter is anywhere in the given word"""
-
+    for ch in word:
+        letter == ch
+        return True
     return False
 
 def inSpot(letter, word, spot):
     """Returns boolean response if letter is in the given spot in the word."""
-
-    return False
-
+    correctLetter = word[spot]
+    if letter == correctLetter:
+        return True
+    else:
+        return False
+     
 def rateGuess(myGuess, word):
     """Rates your guess and returns a word with the following features.
     - Capital letter if the letter is in the right spot
     - Lower case letter if the letter is in the word but in the wrong spot
     - * if the letter is not in the word at all"""
+    feedback = ""
+
+    for spot in range(5):
+        myLetter=myGuess[spot]
+        if inSpot(myLetter, word, spot) == True:
+            feedback = feedback + myLetter.upper()
+        elif inWord(myLetter, word) == True:
+            feedback = feedback + myLetter.lower()
+        else :
+            feedback = feedback + "*"
+            
+    return feedback            
 
 
 def main():
@@ -28,10 +45,28 @@ def main():
     print(todayWord)
 
     #User should get 6 guesses to guess
+    guessNum = 1
+    while guessNum <= 6:
+        validword = False   
+        while validword == False :
+            guess = input("Enter Guess : ")
+            guess = guess.lower()
+            if guess not in wordList:
+                print(" Word not in the list  !! ")
+                validword = False
+            else:
+                validword = True
+                feedback = rateGuess (guess, todayWord)
+                print (feedback)
 
-    #Ask user for their guess
-    #Give feedback using on their word:
+        if feedback == todayWord.upper() :
+            print ("You guessed it in ", guessNum , " tries !!")
+            break
 
+        guessNum = guessNum + 1
+
+    print ("You had ", guessNum , "tries") 
+    print ("The word was : ", todayWord)
 
 
 
