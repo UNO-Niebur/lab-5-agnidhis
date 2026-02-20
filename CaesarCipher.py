3c76e410033bf1f8a8cde907dee8536f38a0da2b
agnidhis <nidhiagarwal@unomaha.edu>
2026-02-20 15:06:09 +0000

    Caesar Cipher Game

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -18,8 +18,18 @@ def encode(message, key):
 
     return secret
 
-#def decode(message, key):
-    #We will want to decode the message here.
+def decode(message, key):
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    message = message.upper()
+    plaintext = ""
+
+    for letter in message:
+        if (alpha.find(letter)>=0):
+            spot = (alpha.find(letter) - key)%26
+            plaintext += alpha[spot]
+        else:
+            plaintext += letter    
+    return plaintext
 
 def main():
     message = input("Enter a message: ")
@@ -27,8 +37,8 @@ def main():
 
     secret = encode(message, key)
     print ("Encrypted:", secret)
-    #plaintext = decode(secret, key)
-    #print ("Decrypted:", plaintext)
+    plaintext = decode(secret, key)
+    print ("Decrypted:", plaintext)
 
 
 if __name__ == '__main__':
